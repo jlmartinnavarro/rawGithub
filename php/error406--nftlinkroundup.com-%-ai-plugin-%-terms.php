7ce7c95d20743f13d98617a68e952e6998f68a55
Jose Luis <jose.martinnavarro@aalto.fi>
2024-06-14 19:11:12 +0300

    fixed links

---
+++ php/error406--nftlinkroundup.com-%-ai-plugin-%-terms.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html data-adblockkey="MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBANDrp2lz7AOmADaN8tA50LsWcjLFyQFcb/P2Txc58oYOeILb3vBw7J6f4pamkAQVSQuqYsKx3YzdUHCvbVZvFUsCAwEAAQ==_pKEkR/cNJfSRiHB5K8MgnjmpNqS103xlzQ6MJWtfVQ1Qg/hU2YtqlKSjczfIWc82Nft1kVsjMOsf/Zr1BT9Lcg==" lang="en" style="background: #2B2B2B;">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAADElEQVQI12P4//8/AAX+Av7czFnnAAAAAElFTkSuQmCC">
+    <link rel="preconnect" href="https://www.google.com" crossorigin>
+</head>
+<body>
+<div id="target" style="opacity: 0"></div>
+<script>window.park = "eyJ1dWlkIjoiYWIzY2Q0MTctZDAzNi00MzI5LWI3YTctZjcxNDRmNGU4OGNkIiwicGFnZV90aW1lIjoxNzE4MzUyODAzLCJwYWdlX3VybCI6Imh0dHBzOi8vbmZ0bGlua3JvdW5kdXAuY29tL2FpLXBsdWdpbi90ZXJtcy5waHAiLCJwYWdlX21ldGhvZCI6IkdFVCIsInBhZ2VfcmVxdWVzdCI6e30sInBhZ2VfaGVhZGVycyI6e30sImhvc3QiOiJuZnRsaW5rcm91bmR1cC5jb20iLCJpcCI6IjE5My4xMC4zOS4xMDIifQo=";</script>
+<script src="/bwomUyKFi.js"></script>
+</body>
+</html>
